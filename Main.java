8ec9d78f55e947a32e4781e1c071d4b53aa99233
joeoakes <jxo19@psu.edu>
2024-01-01 12:13:01 -0500

    commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(2, 3));
+        System.out.println(calculator.subtract(10, 3));
+        System.out.println(calculator.multiply(5, 5));
+    }
+}
