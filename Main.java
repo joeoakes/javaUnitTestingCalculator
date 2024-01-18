2b7443c47d587be69b789d7dd092cfd2f127064b
joeoakes <jxo19@psu.edu>
2024-01-18 15:57:59 -0500

    commit

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args){
         Calculator calculator = new Calculator();
 
-        System.out.println(calculator.add(2, 3));
+        int val1 = 2;
+        int val2 = 3;
+
+        System.out.println(calculator.add(val1, val2));
         System.out.println(calculator.subtract(10, 3));
         System.out.println(calculator.multiply(5, 5));
     }
